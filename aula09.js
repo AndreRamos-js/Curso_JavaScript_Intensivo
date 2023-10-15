87c8c9b4f10d15c310a6d8a2b77a4517d00de102
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-10-15 16:30:46 -0300

    Diferenca entre Pre Incremento e Pos Incremento

---
+++ aula09.js
@@ -0,0 +1,12 @@
+let n=10
+
+console.log(n++) //Pós incremento, primeiro retorna o valor 10 e incrementa 1
+console.log(n++) //Aqui ele finalmente retorna 11
+
+// Para pré incremento é só fazer a lógica inversa
+
+let n1=10
+let n2=20
+
+console.log(n1 + "" + n2) // Dessa forma ao invés de háver a soma entre n1 e n2 e resultar 30, ele tratará como
+                          // Uma soma de strings, resultando em 1020
