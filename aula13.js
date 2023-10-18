c5363d438d50f2024307124b40fef8f25f5fad92
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-10-18 11:40:37 -0300

    Condicional IF e ELSE IF

---
+++ aula13.js
@@ -0,0 +1,14 @@
+
+
+
+let num = 10
+
+if (num > 10){
+    console.log('Numeral maior que 10')
+}else if(num > 5){
+    console.log('Numeral está entre 6 e 10')
+}else{
+    console.log('Numeral igual ou menor que 5')
+}
+
+console.log('Fim do programa')
