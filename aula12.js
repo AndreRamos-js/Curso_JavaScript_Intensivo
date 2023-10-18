d8483f6634548c888def9c17ac7e560e5bd271fd
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-10-17 23:59:12 -0300

    Operador Spread

---
+++ aula12.js
@@ -0,0 +1,24 @@
+
+
+
+/*
+let n1 = [10, 20, 30]
+let n2 = [40, 50, 60]
+let n3 = [...n1, ...n2]
+
+
+console.log('n1: ' + n1)
+console.log('n2: ' + n2)
+console.log('n3: ' + n3)
+*/
+
+const objs1 = document.getElementsByTagName('div')
+const objs2 = [...document.getElementsByTagName('div')]
+
+objs2.forEach(element => { // Dessa forma, como o objs2 fo espalhado com spread, a função forEach irá retornar os valores de objs2
+    console.log(element)
+    });
+
+console.log(objs1)
+console.log(objs2)
+
