440399c07066027706821ee4a9f7a1839afefaf3
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-10-18 12:26:06 -0300

    Switch Case

---
+++ aula14.js
@@ -0,0 +1,22 @@
+
+
+
+let colocacao = 1
+
+switch (colocacao){
+    case 1:
+        console.log('Medalha de Ouro')
+        break;
+    case 2:
+        console.log('Medalha de Prata')
+        break;
+    case 3:
+        console.log('Medalha de Bronze')
+        break;
+    case 4: case 5: case 6:
+        console.log('Medalha de participação')
+        break;
+    default:
+        console.log('Aplausos')
+        break;
+}
