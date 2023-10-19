68767a6370a2fc47ccc287438702cbaa88fdf6c4
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-10-19 14:20:25 -0300

    Introducao ao for

---
+++ aula15.js
@@ -0,0 +1,20 @@
+
+
+
+console.log('Inicio do primeiro programa')
+
+for(let i=0; i < 10; i++){
+    console.log(+i) // Adicionando o +i no console.log é possivel ver a iteração fazendo o numeral ir crescendo
+}
+
+console.log('Fim do primeiro programa')
+
+console.log('Inicio do segundo programa')
+
+for(let i=0; i < 10; i++){
+    if(i%2==0){
+        console.log(+i)
+    }
+}
+
+console.log('Fim do segundo programa')
