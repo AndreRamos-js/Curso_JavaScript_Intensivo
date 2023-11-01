d4508f0979bd4e17179c67b84560a6391cd16f8a
André Ramos <83097746+AndreRamos-py@users.noreply.github.com>
2023-10-31 23:57:49 -0300

    Introducao a funcoes

---
+++ aula20.js
@@ -0,0 +1,13 @@
+
+
+
+function mudarTexto(){
+    let d1=document.getElementById('d1')
+    let d2=document.getElementById('d2')
+    let d3=document.getElementById('d3')
+    d1.innerHTML='Funções'
+    d2.innerHTML='Funções'
+    d3.innerHTML='Funções'
+}
+
+// mudarTexto()
